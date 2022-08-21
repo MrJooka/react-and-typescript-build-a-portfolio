51e84e92edde7cf838a293f8a324b783b7b67036
Jooka <mr.jooka@gmail.com>
2022-08-22 00:16:43 +0900

    37. Adding Request Logic

--- redux-ts/src/state/action-creators/index.ts
+++ redux-ts/src/state/action-creators/index.ts
@@ -9,6 +9,21 @@ const searchRepositories = (term: string) => {
     });
 
     try {
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const names = data.objects.map((result: any) => result.package.name);
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
     } catch (err) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
