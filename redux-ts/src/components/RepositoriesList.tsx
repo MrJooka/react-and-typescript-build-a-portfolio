e120f92950c9c8b43c101417dab445d2b0157475
Jooka <mr.jooka@gmail.com>
2022-08-22 01:30:51 +0900

    43. Reminder on Event Types

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { FormEventHandler, useState } from 'react';
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // form의 submit 이벤트의 기본동작은 페이지 전체를 새로고침해버린다. 그래서 막아야한다
+    event.preventDefault();
+  };
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Saerch</button>
       </form>
     </div>
