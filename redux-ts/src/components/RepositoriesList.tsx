dd2827ff8bc69f12e86c6209547b00174322b323
Jooka <mr.jooka@gmail.com>
2022-08-22 01:41:32 +0900

    45. Calling an Action Creator

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,11 +1,15 @@
-import React, { FormEventHandler, useState } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     // form의 submit 이벤트의 기본동작은 페이지 전체를 새로고침해버린다. 그래서 막아야한다
     event.preventDefault();
+    dispatch(actionCreators.searchRepositories(term) as any);
   };
   return (
     <div>
